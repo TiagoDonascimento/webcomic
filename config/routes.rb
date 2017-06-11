67c9cd02ae794ee137292ba5b13f4204389d5b6b
Tiago Donascimento <tacoscorner@hotmail.com>
2017-06-11 19:44:23 -0400

    finialized the links

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 
 Rails.application.routes.draw do
   get 'articles/index'
+  get 'articles/about'
+  get 'articles/contact'
 
 resources :articles, only: [:index, :new, :show]
 resources :characters, only: [:index, :new, :show]
